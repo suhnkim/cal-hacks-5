c0dd5329d92e43e63f31dbebb50c9c808b30f851
lucyliulee <masssb1@gmail.com>
2018-11-16 15:48:38 -0800

    changed Sean's bad files

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,32 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Link } from 'react-router';
+import logo from './logomeet.svg';
 import './App.css';
-import { Button, ButtonGroup, ButtonDropdown, DropdownItem } from 'reactstrap'
-import Home from './home.js';
-import Filters from './filters.js';
-import {BrowserRouter, Route} from 'react-router-dom';
-import ApiCalendar from 'react-google-calendar-api';
-
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Route exact path="/" component={Home} />
-          <Route path="/filters" component={Filters} />
-        </div>
-      </BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <br></br> Welcome to Meet My Professor
+          </p>
+          <button className = "button" id="login"> <code> Login </code> </button>
+        </header>
+      </div>
     );
   }
 }
-export default App;
 
-/*<ButtonGroup  className="App-link">
-  <Button>Classes</Button>
-  <Button>Departments</Button>
-  <ButtonDropdown title="Dropdown">
-    <DropdownItem eventKey="1">Dropdown link</DropdownItem>
-    <DropdownItem eventKey="2">Dropdown link</DropdownItem>
-  </ButtonDropdown>
-</ButtonGroup>*/
+export default App;
